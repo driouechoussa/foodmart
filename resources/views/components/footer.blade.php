13e4363712cefe7d42d9094a09b0a93565afda12
Ousssama Driouech <thedriwsh@gmail.com>
2025-07-21 18:45:27 +0100

    split and sort html document content to components and personalize inside view/components path

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,128 @@
+   <footer class="py-5">
+      <div class="container-fluid">
+        <div class="row">
+
+          <div class="col-lg-3 col-md-6 col-sm-6">
+            <div class="footer-menu">
+              <img src="{{asset('images/logo.png')}}" alt="logo">
+              <div class="social-links mt-5">
+                <ul class="d-flex list-unstyled gap-2">
+                  <li>
+                    <a href="#" class="btn btn-outline-light">
+                      <img class="footerSocialLinks" src="{{asset('images/icons/facebook.svg')}}" alt="facebook">
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" class="btn btn-outline-light">
+                      <img class="footerSocialLinks" src="{{asset('images/icons/twitter.svg')}}" alt="twitter">
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" class="btn btn-outline-light">
+                      <img src="{{asset('images/icons/youtube.svg')}}" alt="youtube" class="footerSocialLinks">
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" class="btn btn-outline-light">
+                      <img src="{{asset('images/icons/instagram.svg')}}" alt="instagram" class="footerSocialLinks">
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" class="btn btn-outline-light">
+                      <img src="{{asset('images/icons/amazon.svg')}}" alt="amazon" class="footerSocialLinks">
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-2 col-sm-6">
+            <div class="footer-menu">
+              <h5 class="widget-title">Ultras</h5>
+              <ul class="menu-list list-unstyled">
+                <li class="menu-item">
+                  <a href="#" class="nav-link">About us</a>
+                </li>
+                <li class="menu-item">
+                  <a href="#" class="nav-link">Conditions </a>
+                </li>
+                <li class="menu-item">
+                  <a href="#" class="nav-link">Our Journals</a>
+                </li>
+                <li class="menu-item">
+                  <a href="#" class="nav-link">Careers</a>
+                </li>
+                <li class="menu-item">
+                  <a href="#" class="nav-link">Affiliate Programme</a>
+                </li>
+                <li class="menu-item">
+                  <a href="#" class="nav-link">Ultras Press</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-md-2 col-sm-6">
+            <div class="footer-menu">
+              <h5 class="widget-title">Customer Service</h5>
+              <ul class="menu-list list-unstyled">
+                <li class="menu-item">
+                  <a href="#" class="nav-link">FAQ</a>
+                </li>
+                <li class="menu-item">
+                  <a href="#" class="nav-link">Contact</a>
+                </li>
+                <li class="menu-item">
+                  <a href="#" class="nav-link">Privacy Policy</a>
+                </li>
+                <li class="menu-item">
+                  <a href="#" class="nav-link">Returns & Refunds</a>
+                </li>
+                <li class="menu-item">
+                  <a href="#" class="nav-link">Cookie Guidelines</a>
+                </li>
+                <li class="menu-item">
+                  <a href="#" class="nav-link">Delivery Information</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-md-2 col-sm-6">
+            <div class="footer-menu">
+              <h5 class="widget-title">Customer Service</h5>
+              <ul class="menu-list list-unstyled">
+                <li class="menu-item">
+                  <a href="#" class="nav-link">FAQ</a>
+                </li>
+                <li class="menu-item">
+                  <a href="#" class="nav-link">Contact</a>
+                </li>
+                <li class="menu-item">
+                  <a href="#" class="nav-link">Privacy Policy</a>
+                </li>
+                <li class="menu-item">
+                  <a href="#" class="nav-link">Returns & Refunds</a>
+                </li>
+                <li class="menu-item">
+                  <a href="#" class="nav-link">Cookie Guidelines</a>
+                </li>
+                <li class="menu-item">
+                  <a href="#" class="nav-link">Delivery Information</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-6 col-sm-6">
+            <div class="footer-menu">
+              <h5 class="widget-title">Subscribe Us</h5>
+              <p>Subscribe to our newsletter to get updates about our grand offers.</p>
+              <form class="d-flex mt-3 gap-0" role="newsletter">
+                <input class="form-control rounded-start rounded-0 bg-light" type="email" placeholder="Email Address" aria-label="Email Address">
+                <button class="btn btn-dark rounded-end rounded-0" type="submit">Subscribe</button>
+              </form>
+            </div>
+          </div>
+          
+        </div>
+      </div>
+    </footer>
